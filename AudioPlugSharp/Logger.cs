6132bcde9f1ec9a2153e9dc85926880c552e8b09
Mike Oliphant <oliphant@nostatic.org>
2020-11-27 09:18:27 -0800

    Better log handling

--- AudioPlugSharp/Logger.cs
+++ AudioPlugSharp/Logger.cs
@@ -1,41 +1,44 @@
 ﻿using System;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
+using System.Globalization;
 using System.IO;
-using System.Linq;
-using System.Text;
 using System.Threading;
-using System.Threading.Tasks;
 
 namespace AudioPlugSharp
 {
     public class Logger
     {
+        const int NumLogsToRetain = 10;
+
         static ConcurrentQueue<string> logQueue;
         static StreamWriter logWriter = null;
+        static string logPath;
+        static string logFileDateFormat = "yyyy-MM-dd-HH-mm-ss-ffff";
 
         static Logger()
         {
             logQueue = new ConcurrentQueue<string>();
 
+            logPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "AudioPlugSharp");
+
             try
             {
-                string logPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "AudioPlugSharp");
+                DeleteOldLogs();
+            }
+            catch (Exception ex)
+            {
+                Log("Failed to delete old logfiles: " + ex.ToString());
+            }
 
+            try
+            {
                 if (!Directory.Exists(logPath))
                     Directory.CreateDirectory(logPath);
 
-                string logFile = Path.Combine(logPath, "AudioPlugSharp.log");
+                string filename = string.Format("{0:" + logFileDateFormat + "}.log", DateTime.Now);
 
-                if (File.Exists(logFile))
-                {
-                    string oldLogFile = Path.Combine(logPath, "AudioPlugSharp.log.old");
-
-                    if (File.Exists(oldLogFile))
-                        File.Delete(oldLogFile);
-
-                    File.Move(logFile, oldLogFile);
-                }
+                string logFile = Path.Combine(logPath, filename);
 
                 logWriter = new StreamWriter(logFile);
 
@@ -44,11 +47,47 @@ namespace AudioPlugSharp
                 logThread.Priority = ThreadPriority.Lowest;
                 logThread.IsBackground = true;
                 logThread.Start();
-
-                AppDomain.CurrentDomain.ProcessExit += (s, e) => WriteLogs();
             }
             catch
             {
+            }
+        }
+
+        public static void Log(string logEntry)
+        {
+            logQueue.Enqueue(logEntry);
+        }
+
+        static void DeleteOldLogs()
+        {
+            string[] logFiles = Directory.GetFiles(logPath, "*.log");
+
+            if (logFiles.Length >= NumLogsToRetain)
+            {
+                Dictionary<string, long> fileTimes = new Dictionary<string, long>();
+
+                foreach (string logFile in logFiles)
+                {
+                    DateTime fileTime = DateTime.MinValue;
+
+                    DateTime.TryParseExact(Path.GetFileNameWithoutExtension(logFile), logFileDateFormat, CultureInfo.InvariantCulture, DateTimeStyles.None, out fileTime);
+
+                    fileTimes[logFile] = fileTime.Ticks;
+                }
+
+                Array.Sort(logFiles, delegate (string file1, string file2)
+                {
+                    return fileTimes[file2].CompareTo(fileTimes[file1]);
+                });
+            }
+
+            for (int i = NumLogsToRetain - 1; i < logFiles.Length; i++)
+            {
+                try
+                {
+                    File.Delete(logFiles[i]);
+                }
+                catch { }
             }
         }
 
@@ -69,23 +108,19 @@ namespace AudioPlugSharp
         static void WriteLogs()
         {
             string logEntry;
-
             bool wroteEntries = false;
 
             while (logQueue.TryDequeue(out logEntry))
             {
-                logWriter.WriteLine(logEntry);
+                string logLine = string.Format("[{0:yyyy/MM/dd HH:mm:ss:ffff}] {1}", DateTime.Now, logEntry);
+
+                logWriter.WriteLine(logLine);
 
                 wroteEntries = true;
             }
 
             if (wroteEntries)
                 logWriter.Flush();
-        }
-
-        public static void Log(string logEntry)
-        {
-            logQueue.Enqueue(logEntry);
         }
     }
 }
