a2b1714753f5693532c061526676456711e9919c
Mike Oliphant <oliphant@nostatic.org>
2021-01-08 10:42:13 -0800

    Expose editor window display rectangle

--- AudioPlugSharpWPF/EditorWindow.cs
+++ AudioPlugSharpWPF/EditorWindow.cs
@@ -1,19 +1,38 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Text;
 using System.Windows;
 using System.Windows.Controls;
+using System.Runtime.InteropServices;
 using AudioPlugSharp;
 
 namespace AudioPlugSharpWPF
 {
     public class EditorWindow : Window
     {
-        [System.Runtime.InteropServices.DllImport("user32.dll", SetLastError = true)]
+        [DllImport("user32.dll", SetLastError = true)]
         static extern IntPtr SetParent(IntPtr hWndChild, IntPtr hWndNewParent);
+
+        [DllImport("user32.dll")]
+        public static extern bool GetWindowRect(IntPtr hwnd, ref Rectangle rectangle);
 
         public IAudioPluginEditor Editor { get; private set; }
         public UserControl EditorView { get; private set; }
+
+        public Rectangle DisplayRectangle
+        {
+            get
+            {
+                Rectangle displayRectangle = new Rectangle();
+
+                GetWindowRect(parentWindow, ref displayRectangle);
+
+                return displayRectangle;
+            }
+        }
+
+        IntPtr parentWindow;
 
         public EditorWindow(IAudioPluginEditor editor, UserControl editorView)
         {
@@ -25,6 +44,8 @@ namespace AudioPlugSharpWPF
 
         public void Show(IntPtr parentWindow)
         {
+            this.parentWindow = parentWindow;
+
             Content = EditorView;
 
             Top = 0;
